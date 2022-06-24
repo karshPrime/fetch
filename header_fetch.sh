83651ec1ef25c1b808d62b8da21c53b6e646e805
ut-kr <utkarsh@cyberfear.com>
2022-06-24 19:24:16 +1000

    ✨ chmod +x

--- header_fetch.sh
+++ header_fetch.sh
@@ -6,6 +6,7 @@ MEMORY=$(free -m | grep Mem | awk '{print $3"MiB / "$2"MiB" }')
 PACKAGES=$(pacman -Qq | wc -l)
 BATTERY=$(cat /sys/class/power_supply/BAT1/capacity)
 SEP='•'
+
 # Uptime
 if [ $(uptime -p | wc -w) == '3' ]; then
 	UPTIME=$(uptime -p | awk '{print $2,"mins"}')
@@ -13,8 +14,8 @@ else
 	UPTIME=$(uptime -p | awk '{print $2"hr(s) "$4"mins "}')
 fi
 
-echo -e "\e[35m Uptime: \e[39m$UPTIME \e[36m$SEP \
-\e[31m  Charge: \e[39m$BATTERY% \e[36m$SEP \
+echo -e "\e[31m Uptime: \e[39m$UPTIME \e[36m$SEP \
+\e[35m  Charge: \e[39m$BATTERY% \e[36m$SEP \
 \e[33m Pkgs: \e[39m$PACKAGES \e[36m$SEP \
 \e[34m Mem: \e[39m$MEMORY \e[36m$SEP \
 \e[32m Disk: \e[39m$STORAGE"
