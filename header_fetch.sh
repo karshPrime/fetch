6e38900d6d697bbb1d768022319a6f0d1b4bbc75
karsh <utkarsh@cyberfear.com>
2023-12-15 13:19:28 +1100

    ✨ nerd Icons

--- header_fetch.sh
+++ header_fetch.sh
@@ -1,21 +1,18 @@
-#!/usr/bin/env bash
+#!/usr/bin/env/ bash
 
 # information to display
-STORAGE=$(df -h | grep nvme0n1p3 | awk '{print $3"/"$2" ["$5"]"}')
-MEMORY=$(free -m | grep Mem | awk '{print $3"MiB / "$2"MiB" }')
+STORAGE=$(df -h | grep n1p2 | awk '{print $3"/"$2" ["$5"]"}')
+MEMORY=$(free -m | grep Mem | awk '{print $3"MiB/"$2"MiB" }')
 PACKAGES=$(pacman -Qq | wc -l)
 BATTERY=$(cat /sys/class/power_supply/BAT1/capacity)
+UPTIME=$(uptime | awk '{print $3}' | sed 's/,//')
 SEP='•'
 
-# Uptime
-if [ $(uptime -p | wc -w) == '3' ]; then
-	UPTIME=$(uptime -p | awk '{print $2,"mins"}')
-else
-	UPTIME=$(uptime -p | awk '{print $2"hr(s) "$4"mins "}')
-fi
-
-echo -e "\e[31m Uptime: \e[39m$UPTIME \e[36m$SEP \
+echo -e "\e[31m  Uptime: \e[39m$UPTIME \e[36m$SEP \
 \e[35m  Charge: \e[39m$BATTERY% \e[36m$SEP \
-\e[33m Pkgs: \e[39m$PACKAGES \e[36m$SEP \
-\e[34m Mem: \e[39m$MEMORY \e[36m$SEP \
-\e[32m Disk: \e[39m$STORAGE"
+\e[33m  Pkgs: \e[39m$PACKAGES \e[36m$SEP \
+\e[34m  Mem: \e[39m$MEMORY \e[36m$SEP \
+\e[32m  Disk: \e[39m$STORAGE"
+
+echo ""
+echo ""
