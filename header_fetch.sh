cc7e5f1af4340bed34c8cb9170893268585dfe24
karsh <utkarsh@cyberfear.com>
2023-12-19 11:38:24 +1100

    ⚙ dd-hh:mm uptime

--- header_fetch.sh
+++ header_fetch.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env/ bash
 
 # information to display
+SEP='•'
 STORAGE=$(df -h | grep n1p2 | awk '{print $3"/"$2" ["$5"]"}')
 MEMORY=$(free -m | grep Mem | awk '{print $3"MiB/"$2"MiB" }')
 PACKAGES=$(pacman -Qq | wc -l)
 BATTERY=$(cat /sys/class/power_supply/BAT1/capacity)
-UPTIME=$(uptime | awk '{print $3}' | sed 's/,//')
-SEP='•'
+UPTIME=$(
+    uptime | awk -F'( |,|:)+' '{
+    d=h=m=0;
+    if ($7=="min") m=$6;
+    else {
+	if ($7~/^day/) {
+            d=$6;
+	    h=$8;
+	    m=$9
+	}
+        else {
+	    h=$6;
+	    m=$7
+        }
+    }
+    printf "%d-%02d:%02d\n", d+0, h+0, m+0
+    }' | tr -d ' '
+)
 
 echo -e "\e[31m  Uptime: \e[39m$UPTIME \e[36m$SEP \
 \e[35m  Charge: \e[39m$BATTERY% \e[36m$SEP \
